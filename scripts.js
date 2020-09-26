4ddd2a07e2a075a986810220f50dd02ad8746f50
Gustavo G <gustavogoullart@hotmail.com>
2020-09-26 12:39:19 -0300

    checking to dolist beginning

--- scripts.js
+++ scripts.js
@@ -31,8 +31,24 @@ function renderTodo(todo) {
   const list = document.querySelector('.js-todo-list');
 
   const listItem = document.createElement('li');
+  listItem.setAttribute('data-key', `${todo.id}`)
 
-  listItem.innerHTML = `<span>${todo.text}</span>`;
+  listItem.setAttribute('class', 'todo-item')
+  listItem.innerHTML = `
+  <input id="${todo.id}" type="checkbox"/>
+  <label id="${todo.id}" class="tick js-tick"></label>
+  <span>${todo.text}</span>
+  <button class="delete-todo js-delete-todo">
+    <svg><use href="#delete-icon"></use></svg>
+  </button>
+  `;
 
   list.append(listItem);
 }
+
+const list = document.querySelector('.js-todo-list');
+list.addEventListener('click', e => {
+  if (e.target.classList.contains('js-tick')) {
+
+  }
+})
